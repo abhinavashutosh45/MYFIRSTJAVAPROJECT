b927579f232742591bb8a32353cb4d423da07e38
abhinavashutosh45 <abhinavashutosh37@gmail.com>
2026-03-15 16:42:05 +0530

    Added code and classes A and B

--- src/com/company/cwh_48_method_overriding.java
+++ src/com/company/cwh_48_method_overriding.java
@@ -1,8 +1,33 @@
 package com.company;
 
+class A{
+    public int a;
+    public int harry(){
+        return 4;
+    }
+    public void meth2(){
+        System.out.println("I am method 2 of class A");
+    }
+}
+
+class B extends A{
+    @Override
+    public void meth2() {
+        System.out.println("I am method 2 of class B");
+    }
+    public void meth3(){
+        System.out.println("I am method 3 of class B");
+    }
+}
+
 public class cwh_48_method_overriding {
     static void main(String[] args) {
+        A a = new A();
+        a.meth2();
+
+        B b = new B();
+        b.meth2();
+        b.meth3();
 
     }
-
 }
