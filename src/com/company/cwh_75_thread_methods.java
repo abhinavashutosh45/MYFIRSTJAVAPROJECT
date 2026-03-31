a296e2aba78e7fb09463425f5d0646e4b29fd7d2
abhinavashutosh45 <abhinavashutosh37@gmail.com>
2026-03-31 19:39:17 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/com/company/cwh_75_thread_methods.java
@@ -0,0 +1,55 @@
+package com.company;
+
+class MyNewThr1 extends Thread {
+    public void run() {
+        int i = 0;
+        while (true) {
+            System.out.println("Thank You: ");
+            try{
+                Thread.sleep(455);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            i++;
+        }
+    }
+}
+
+class MyNewThr2 extends Thread {
+    public void run() {
+        while(true) {
+            System.out.println("My Thank You: ");
+        }
+    }
+}
+
+public class cwh_75_thread_methods {
+    static void main(String[] args) {
+        MyNewThr1 t1 = new MyNewThr1();
+        MyNewThr2 t2 = new MyNewThr2();
+        t1.start();
+        /*
+        try {
+            t1.join(); // -- join() method will not start t2 till t1 ends
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+         */
+
+        t2.start(); //-- Starts the thread.
+        /*
+        t1.start(); //-- Starts the thread.
+        t1.getName(); //--it returns the name of the thread.
+        t1.getState(); //-- it returns the state of the thread.
+        t1.getPriority(); //-- it returns the priority of the thread.
+        t1.sleep(); //-- Stop the thread for the specified time.
+        t1.join(); //-- Stop the current thread until the called thread gets terminated
+
+         */
+
+      //  Go to this website for complete thread reading : -->  https://docs.oracle.com/javase/8/docs/api/java/lang/Thread.html
+
+
+    }
+}
