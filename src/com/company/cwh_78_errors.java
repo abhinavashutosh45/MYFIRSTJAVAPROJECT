e71ae7434c6bf04d8ff7b79a8046ee3628660f5e
abhinavashutosh45 <abhinavashutosh37@gmail.com>
2026-04-03 01:24:11 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/company/cwh_75_thread_methods.java

---
+++ src/com/company/cwh_78_errors.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class cwh_78_errors {
+    static void main(String[] args) {
+      /*  Syntax Errors
+        Logical Errors
+        Runtime Errors --> Also called Exception
+
+       */
+    }
+}
