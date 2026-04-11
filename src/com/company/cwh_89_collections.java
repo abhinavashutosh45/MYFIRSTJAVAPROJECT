f7c7cbd581f8d01b58f7ac2bbcfc2c3d968715d5
abhinavashutosh45 <abhinavashutosh37@gmail.com>
2026-04-12 03:51:09 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/company/cwh_75_thread_methods.java

---
+++ src/com/company/cwh_89_collections.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class cwh_89_collections {
+    static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        TreeSet<Integer> treeSet = new TreeSet<>();
+    }
+}
