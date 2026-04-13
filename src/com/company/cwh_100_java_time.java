241696950097e925d2df64e08ebf837c48982e98
abhinavashutosh45 <abhinavashutosh37@gmail.com>
2026-04-14 01:20:51 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/company/cwh_75_thread_methods.java

---
+++ src/com/company/cwh_100_java_time.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class cwh_100_java_time {
+    static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println(dateTime);
+    }
+}
