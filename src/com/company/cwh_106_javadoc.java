6ffa881fa1afc3707d96efd1f8ffba827a6344ca
abhinavashutosh45 <abhinavashutosh37@gmail.com>
2026-04-17 05:05:18 +0530

    Merge remote-tracking branch 'gitlab/master'

---
+++ src/com/company/cwh_106_javadoc.java
@@ -0,0 +1,21 @@
+package com.company;
+
+/**
+ * This class is to demonstrate what Javadoc is and how it is used in the java industry
+ * This is <i>italic</i> word<p>this is a new paragraph</p>
+ * @author Ashutosh (CodeWithAbhi)
+ * @version 0.1
+ * @since 2002
+ * @see <a href="https://docs.oracle.com/en/java/javase/26/docs/api/index.html" target="_blank">Java Docs</a>
+ */
+
+public class cwh_106_javadoc {
+
+    public void add(int a, int b){
+        System.out.println("The sum is: " + a+b);
+    }
+
+    static void main(String[] args) {
+        System.out.println("This is my main method");
+    }
+}
